9cd7e0937fd6917b000650d49fcd72d2c2005c28
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-26 13:45:50 -0500

    mn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GCDCalculator gcd = (a, b) -> {
+            while (b != 0) {
+                int oldB = b;
+                b = a % b;
+                a = oldB;
+            }
+            return a;
+        };
+        System.out.println(gcd.gcd(25, 20));
+    }
+
+    @FunctionalInterface
+    public interface GCDCalculator {
+        int gcd(int a, int b);
+
     }
 }
